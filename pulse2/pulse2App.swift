64c7ecd1adb6689d42b09e16685679bccd04da8b
isiflo2728 <iflore110@calstatela.edu>
2025-09-02 17:33:11 -0700

    iniital Commit

--- pulse2/pulse2App.swift
+++ pulse2/pulse2App.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct pulse2App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(
+                //Help
+            )
         }
     }
 }
