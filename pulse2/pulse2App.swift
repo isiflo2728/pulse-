ff9b51d29ea1d5ee5fcdc4a3472e56bbb5bce84f
isiflo2728 <iflore110@calstatela.edu>
2025-09-02 17:26:43 -0700

    Initial Commit

---
+++ pulse2/pulse2App.swift
@@ -0,0 +1,17 @@
+//
+//  pulse2App.swift
+//  pulse2
+//
+//  Created by Isidoro Flores on 9/2/25.
+//
+
+import SwiftUI
+
+@main
+struct pulse2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pulse2Tests/pulse2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  pulse2Tests.swift
+//  pulse2Tests
+//
+//  Created by Isidoro Flores on 9/2/25.
+//
+
+import Testing
+@testable import pulse2
+
+struct pulse2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
