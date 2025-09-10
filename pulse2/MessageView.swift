374aa49b4f948b2a1f51ff28440073b953440f7e
isiflo2728 <iflore110@calstatela.edu>
2025-09-09 17:54:01 -0700

    Message View

--- pulse2/MessageView.swift
+++ pulse2/MessageView.swift
@@ -3,20 +3,41 @@
 //  pulse2
 //
 //  Created by Isidoro Flores on 9/5/25.
-//
+
 
 import SwiftUI
 
+
+
 struct MessageView: View {
+@State private var searchText: String = ""
+    
+class Model: ObservableObject {
+        @Published var searchText: String = ""
+    }
+
     var body: some View {
+        
         
         NavigationStack{
             Form{
                 Section{
                     Text("Hello World!!")
+                    Text("Sample message 1")
+                    Text("Sample message 2")
+                    Text("Another message")
                 }
             }
-            .navigationTitle("Messages")
+            .navigationTitle("Messages").searchable(text: $searchText, prompt: "Search messages").toolbar{
+                ToolbarItem(placement:.navigationBarTrailing){
+                    Button(action:{
+                        print("Hello World!!")
+                        
+                    }){
+                        Image(systemName: "pencil")
+                    }
+                }
+            }
             
         }
         
